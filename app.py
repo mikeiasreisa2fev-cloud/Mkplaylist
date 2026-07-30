0ad95b750b7f9fd41b2411ee6e44244b0ec7a687
mikeiasreisa2fev-cloud <mikeiasreis.a2fev@gmail.com>
2026-07-30 01:01:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    h = request.host_url
-    return f"<h1>SpeedFlix Master Online</h1><p>M3U: {h}playlist.m3u<br>EPG: {h}epg.xml</p>"
+    return "<h1>SpeedFlix Online</h1>"
 
 @app.route("/playlist.m3u")
-def serve_m3u():
+def playlist():
     if os.path.exists("playlist.m3u"):
         with open("playlist.m3u", "r", encoding="utf-8") as f:
             return Response(f.read(), mimetype="text/plain")
-    return "#EXTM3U\n# Erro: Aguarde o GitHub gerar a lista.", 200
+    return "#EXTM3U\n# Erro: Lista ainda nao gerada.", 200
 
 @app.route("/epg.xml")
-def serve_epg():
+def epg():
     if os.path.exists("epg.xml"):
         with open("epg.xml", "r", encoding="utf-8") as f:
             return Response(f.read(), mimetype="application/xml")
